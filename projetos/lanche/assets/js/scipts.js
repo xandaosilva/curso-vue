68120200ea172f1670d6719328a5c3d25c4dc641
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-04-28 18:28:55 -0300

    Definindo as imagens como propriedades computadas

--- projetos/lanche/assets/js/scipts.js
+++ projetos/lanche/assets/js/scipts.js
@@ -1,5 +1,19 @@
 const vm = new Vue({
     el: "#app",
     data: {},
-    methods: {}
+    methods: {},
+    computed: {
+        pao(){
+            return ["assets/img/padrao/pao_superior.png", "assets/img/padrao/pao_inferior.png"];
+        },
+        alface(){
+            return "assets/img/padrao/alface.png";
+        },
+        molho(){
+            return "assets/img/padrao/molho.png";
+        },
+        hamburger(){
+            return "assets/img/padrao/hamburguer.png";
+        }
+    }
 })
