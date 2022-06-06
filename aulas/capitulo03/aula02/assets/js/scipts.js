52632a43ccf57e99a90797b58cf208a9c6e5cc3f
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-06-06 04:23:31 -0300

    Utilizando arrays para definir estilos visuais por meio do atributo class

---
+++ aulas/capitulo03/aula02/assets/js/scipts.js
@@ -0,0 +1,10 @@
+const vm = new Vue({
+    el: "#app",
+    data: {
+        estilo1: 'vermelho',
+        estilo2: 'texto-branco',
+        estilo3: 'destaque',
+        ehDestaque: false
+    },
+    methods: {}
+})
