d6395ca17a813c40f7cf1915591ca346d2c19575
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-06-06 18:37:54 -0300

    Utilizando objetos para definir estilos visuais por meio do atributo style

---
+++ aulas/capitulo03/aula03/assets/js/scipts.js
@@ -0,0 +1,15 @@
+const vm = new Vue({
+    el: "#app",
+    data: {
+        estilo: {
+            background: "blue",
+            width: 250 + "px", 
+            height: 150 + "px", 
+            padding: 10 + "px",
+            color: "white",
+            fontSize: 20 + "px",
+            fontWeight: "bold"
+        }
+    },
+    methods: {}
+})
