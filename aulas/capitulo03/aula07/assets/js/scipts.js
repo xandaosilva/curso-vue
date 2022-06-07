0f2c18027bfbd46dc6f94f3b70eb72d173144712
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-06-07 12:28:51 -0300

    Carregando o template por meio do método

---
+++ aulas/capitulo03/aula07/assets/js/scipts.js
@@ -0,0 +1,10 @@
+const vm = new Vue({
+    data: {
+        texto1: "Template",
+        texto2: "Formas alternativas de carregar o template"
+    },
+    methods: {},
+    computed:{}
+})
+
+vm.$mount("#app");
