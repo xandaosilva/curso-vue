5af14d297ccb0c22d9f7e206e42e470e59416c6c
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-06-07 12:34:13 -0300

    Carregando o template por meio da propriedade template

--- aulas/capitulo03/aula07/assets/js/scipts.js
+++ aulas/capitulo03/aula07/assets/js/scipts.js
@@ -1,4 +1,10 @@
 const vm = new Vue({
+    template: `
+        <div>
+            <h1>{{ texto1 }}</h1>
+            <h1>{{ texto2 }}</h1>
+        </div>
+    `,
     data: {
         texto1: "Template",
         texto2: "Formas alternativas de carregar o template"
