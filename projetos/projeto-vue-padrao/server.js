f617bf177af914b691c7e3c14686079b5e697de6
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-06-20 11:11:11 -0300

    Pasta dist para produção do projeto

--- projetos/projeto-vue-padrao/server.js
+++ projetos/projeto-vue-padrao/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const porta = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + "/dist/"))/
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/dist/index.html"));
 
-app.listen(3000, () => console.log("Servidor web funcionando na porta 3000"));
+app.listen(porta, () => console.log(`Servidor web funcionando na porta ${porta}`));
