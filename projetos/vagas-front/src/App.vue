a0c927525b1e0b979675ece32435f2ede137747a
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-06-24 17:26:12 -0300

    Criando os componentes Topo e Conteudo, adicionando o Bootstrap no projeto e customizando o componente Topo

--- projetos/vagas-front/src/App.vue
+++ projetos/vagas-front/src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Topo />
+    <Conteudo />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Conteudo from './components/layouts/Conteudo.vue'
+import Topo from './components/layouts/Topo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Conteudo,
+    Topo
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
+} */
 </style>
---
+++ projetos/vagas-front/src/components/layouts/Conteudo.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Conteúdo</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Conteudo'
+}
+</script>
+
+<style>
+</style>
