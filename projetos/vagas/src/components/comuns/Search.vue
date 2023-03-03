4c7b96aa2dae9276c9ec95909fda464b9df66d9e
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-03-03 19:29:00 +0044

    Component Seacrh

---
+++ projetos/vagas/src/components/comuns/Search.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="h-100 p-5 bg-light border rounded-3">
+        <h2>Pesquisar vagas</h2>
+        <div class="row">
+            <div class="col">
+                <div class="form-group">
+                    <label for="">Título da vaga</label>
+                    <input type="text" class="form-control" placeholder="Pesquise por palavras chaves, por exemplo 'Python', 'Pleno' e 'Analista'">
+                    <small class="form-text text-muted">Informe palavras que estejam relacionadas com o título da vaga que você procura</small>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <button class="btn btn-outline-dark mt-2" type="button">Buscar</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "Search"
+    }
+</script>
+
+<style></style>
--- projetos/vagas/src/components/views/Home.vue
+++ projetos/vagas/src/components/views/Home.vue
@@ -2,23 +2,7 @@
     <div class="container py-4">
         <div class="row">
             <div class="col">
-                <div class="h-100 p-5 bg-light border rounded-3">
-                    <h2>Pesquisar vagas</h2>
-                    <div class="row">
-                        <div class="col">
-                            <div class="form-group">
-                                <label for="">Título da vaga</label>
-                                <input type="text" class="form-control" placeholder="Pesquise por palavras chaves, por exemplo 'Python', 'Pleno' e 'Analista'">
-                                <small class="form-text text-muted">Informe palavras que estejam relacionadas com o título da vaga que você procura</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                    <div class="col">
-                        <button class="btn btn-outline-dark mt-2" type="button">Buscar</button>
-                    </div>
-                </div>
-                </div>
+                <Search />
             </div>
         </div>
         <div class="row mt-5">
@@ -45,8 +29,13 @@
 </template>
 
 <script>
+    import Search from '@/components/comuns/Search.vue';
+
     export default{
-        name: "Home"
+        name: "Home",
+        components: {
+            Search
+        }
     }
 </script>
 
