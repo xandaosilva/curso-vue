4e6ff6b8eac8de7d0750bf6bfba553378f6868da
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-03-29 14:37:52 +0044

    Manipulando HTML através da diretiva customizada

--- projetos/diretivas/src/App.vue
+++ projetos/diretivas/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <p v-teste>Minha primeira diretiva customizada</p>
+  <div>
+    <p v-teste>Minha primeira diretiva customizada</p>
+    <p v-texto>Teste diretiva de texto</p>
+    <p v-texto>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium expedita laboriosam quibusdam reprehenderit, quisquam enim sed cumque velit sit, nihil voluptas eius officia doloremque facilis. Minima unde non numquam quisquam?</p>
+  </div>
 </template>
 
 <script>
--- projetos/diretivas/src/main.js
+++ projetos/diretivas/src/main.js
@@ -1,12 +1,37 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-
 const Vue = createApp(App);
 
 Vue.directive("teste", {
     created(){
         console.log("A diretiva foi aplicada com sucesso.");
+    }
+});
+
+Vue.directive("texto", {
+    created(el){
+        el.style.color = "cyan";
+        el.style.fontSize = "2rem";
+        
+        let text = el.innerText;
+        let textLength = text.length;
+        let textModified = "";
+
+        if(textLength > 25){
+            textModified = text.substring(0, 22)  + "...";
+        }
+        else{
+            textModified = text;
+        }
+
+
+        el.innerText = textModified;
+
+        console.log(el);
+        console.log("Texto original:", text);
+        console.log("Tamanho do texto:", textLength);
+        console.log("Texto modificado:", textModified);
     }
 });
 
