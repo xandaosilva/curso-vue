fb1f37f95979fdea50d724b79dcb0f47542b1aba
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-04-02 03:44:47 +0044

    Múltiplos mixins

--- projetos/mixins/src/components/ListMovies.vue
+++ projetos/mixins/src/components/ListMovies.vue
@@ -3,6 +3,7 @@
         <h3>Lista de Filmes</h3>
         <input type="text" placeholder="Nome do filme" v-model="item">
         <button type="button" @click="addItem">Adicionar</button>
+        <button type="button" @click="removeItem">Remover</button>
         <ul>
             <li v-for="(item, index) in items" :key="index">{{ item }}</li>
         </ul>
@@ -11,10 +12,11 @@
 
 <script>
     import ListMixin from '@/mixins/ListMixin.js';
+    import RemoveItemListMixin from '@/mixins/RemoveItemListMixin';
 
     export default{
         name: "ListMovie",
-        mixins: [ListMixin]
+        mixins: [ListMixin, RemoveItemListMixin]
     }
 </script>
 
--- projetos/mixins/src/components/ListaPeople.vue
+++ projetos/mixins/src/components/ListaPeople.vue
@@ -3,6 +3,7 @@
         <h3>Lista de Pessoas</h3>
         <input type="text" placeholder="Nome da pessoa" v-model="item">
         <button type="button" @click="addItem">Adicionar</button>
+        <button type="button" @click="removeItem">Remover</button>
         <ul>
             <li v-for="(item, index) in items" :key="index">{{ item }}</li>
         </ul>
@@ -11,10 +12,11 @@
 
 <script>
     import ListMixin from '@/mixins/ListMixin.js';
+    import RemoveItemListMixin from '@/mixins/RemoveItemListMixin';
 
     export default{
         name: "ListPeople",
-        mixins: [ListMixin]
+        mixins: [ListMixin, RemoveItemListMixin]
     }
 </script>
 
