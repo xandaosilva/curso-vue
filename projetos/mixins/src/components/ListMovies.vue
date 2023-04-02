e035489423af3e5071b9139a4b2c568cd08a881b
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-04-02 02:43:53 +0044

    Adicionando e listando registros de filmes

--- projetos/mixins/src/components/ListMovies.vue
+++ projetos/mixins/src/components/ListMovies.vue
@@ -1,10 +1,27 @@
 <template>
-    <h3>Lista de Filmes</h3>
+    <div>
+        <h3>Lista de Filmes</h3>
+        <input type="text" placeholder="Nome do filme" v-model="movie">
+        <button type="button" @click="addMovie">Adicionar</button>
+        <ul>
+            <li v-for="(movie, index) in movies" :key="index">{{ movie }}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
     export default{
-        name: "ListMovie"
+        name: "ListMovie",
+        data: () => ({
+            movies: [],
+            movie: ""
+        }),
+        methods:{
+            addMovie(){
+                this.movies.push(this.movie);
+                this.movie = "";
+            }
+        }
     }
 </script>
 
