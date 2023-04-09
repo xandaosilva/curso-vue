c82acd102c58fdf15283162d260d070893c897e0
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-04-09 02:25:27 +0044

    Ajustes iniciais do projeto

--- projetos/pokedex/src/views/Home.vue
+++ projetos/pokedex/src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row mt-2">
+      <div class="col mb-2">Palco</div>
+      <div class="col mb-2">Pokedex</div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home'
   }
-}
 </script>
