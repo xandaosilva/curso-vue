8dddfe1306f7b0c9147b74517aa39fa7568dee68
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-04-12 10:58:51 +0044

    Listando carros, telefones e kits de reanimação

--- projetos/emergenciasmedicas/src/components/ListaItens.vue
+++ projetos/emergenciasmedicas/src/components/ListaItens.vue
@@ -20,13 +20,19 @@ export default {
         ...mapState({
             enfermeiros: state => state.enfermeiros,
             socorristas: state => state.socorristas,
-            medicos: state => state.medicos
+            medicos: state => state.medicos,
+            carros: state => state.equipamentos.carros,
+            telefones: state => state.equipamentos.telefones,
+            kitsDeReanimacao: state => state.equipamentos.kitsDeReanimacao
         }),
         itens(){
             switch(this.tipo){
                 case "enfermeiros": return this.enfermeiros;
                 case "socorristas": return this.socorristas;
                 case "medicos": return this.medicos;
+                case "carros": return this.carros;
+                case "telefones": return this.telefones;
+                case "kits-de-reanimacao": return this.kitsDeReanimacao;
             }
             return [];
         }
