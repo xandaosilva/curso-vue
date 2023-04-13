6a414162ae77551c0a230b8274df53db60d9d9ba
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-04-13 06:36:30 +0044

    Definindo a imagem da ambulância

--- projetos/emergenciasmedicas/src/components/ConfiguracaoEquipe.vue
+++ projetos/emergenciasmedicas/src/components/ConfiguracaoEquipe.vue
@@ -17,7 +17,7 @@
       <div class="col-4 text-center">
         <div class="row">
           <div class="col">
-            <img class="img-fluid" :src="require('@/assets/ambulancias/indefinida.png')">
+            <img class="img-fluid" :src="require(`@/assets/ambulancias/${imgAmbulancia}`)">
           </div>
         </div>
         <div class="row mt-3">
@@ -56,6 +56,15 @@ export default {
         return "text-danger";
       }
       return "text-primary";
+    },
+    imgAmbulancia(){
+      if(this.kitDeReanimacao){
+        return "uti.png";
+      }
+      if(this.carro){
+        return "simples.png";
+      }
+      return "indefinida.png";
     }
   }
   // computed: mapState(['equipe'])
